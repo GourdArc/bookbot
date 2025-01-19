d9c755f09e34b17ab3cc16d11e68027c5c93c692
GourdArc <soysebi96@gmail.com>
2025-01-19 02:54:19 -0500

    finished basic bookbot

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    
+    print(f"--- Begin report of {book_path} ---")
+    
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
-    num_unique_letters = get_num_letters(text)
-    print(f" {num_unique_letters} is the count for each character in the text")
+    
+    chars = get_num_letters(text)
+    for char_dict in chars:
+        print(f"The '{char_dict['char']}' character was found {char_dict['num']} times")
+
+    print("--- End Report ---")
     
 def get_book_text(path):
+    #opens the text as a string
     with open(path) as f:
         return f.read()
 
 def get_num_words(text):
+    #word count for text
     words = text.split()
     return len(words)
 
@@ -19,13 +27,25 @@ def get_num_letters(text):
     lowered_text = text.lower()
     unique = set()
     unique_count = {}
+    #creates a dictionary that contains a letter and the number of times it appears in the text
     for w in lowered_text:
-        if w not in unique:
+        if w not in unique and w.isalpha():
             unique.add(w)
             unique_count[w] = 1
-        elif w in unique:
+        elif w in unique and w.isalpha():
             unique_count[w] += 1
-    return unique_count
+    #Convert dictionary to list of dictionaries
+    chars = []
+    for char, num in unique_count.items():
+        char_dict = {"char": char, "num": num}
+        chars.append(char_dict)
+    
+    #sort list by num
+    def sort_on(dict):
+        return dict["num"]
+    
+    chars.sort(reverse=True, key=sort_on)
+    return chars
 
 
 
